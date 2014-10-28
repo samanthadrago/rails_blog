a740db02c483bfc7098fd70b6b1ed05f11358f23
Samantha Drago <sfdrago@Samanthas-MacBook-Air-2.local>
2014-10-28 16:21:03 -0700

    did some styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "ddh", password: "secret", except: [:index, :show]
+  # http_basic_authenticate_with name: "ddh", password: "secret", except: [:index, :show]
   def new
     @article = Article.new
   end
-  
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -13,36 +13,36 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def index
     @articles = Article.all
   end
-  
+
   def edit
     @article = Article.find(params[:id])
   end
-  
+
   def update
     @article = Article.find(params[:id])
-    
+
     if @article.update(article_params)
       redirect_to @article
     else
       render 'edit'
     end
   end
-  
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    
+
     redirect_to articles_path
   end
-    
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
